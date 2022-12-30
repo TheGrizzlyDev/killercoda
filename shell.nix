fcafafda039ccbdc49bee998d45b5bc4ca95ec04
Antonio <tonio9681@gmail.com>
2022-12-30 15:29:56 +0000

    WIP work on the java tutorial

--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@
 with pkgs;
 
 mkShell {
-    packages = [ bazelisk bazel-buildtools python3 ];
+    packages = [ bazelisk bazel-buildtools python3 jdk ];
 
     shellHook = ''
         alias bazel=bazelisk
